075be74d58d5ad666962a8e5671be2ea35bb852b
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-26 12:18:31 -0300

    breadcrumbs IV

--- frontend/src/components/Breadcrumbs.jsx
+++ frontend/src/components/Breadcrumbs.jsx
@@ -5,21 +5,23 @@ export default function Breadcrumbs({ category }) {
     const [state, setState] = useState([]);
 
     useEffect(() => {
-        
+
         (async () => {
             const result = await axios(
                 `https://api.mercadolibre.com/categories/${category}`,
             );
             setState(result.data.path_from_root);
         })();
-        
+
     }, [category])
 
     return (
-        <div className="container breadcrumbs">
-            {(state !== undefined) ?
-                state.map(elem => <span> &gt; {elem.name}</span>) : null
-            }
+        <div className="container">
+            <div className="breadcrumbs">
+                {(state !== undefined) ?
+                    state.map(elem => <span>&gt;&nbsp;{elem.name}&nbsp;</span>) : null
+                }
+            </div>
         </div>
     )
 }
