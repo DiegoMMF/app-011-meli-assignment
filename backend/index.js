98714ff6954462a298493f5c5dd06a8c9212985d
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-27 08:47:45 -0300

    moving

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,8 @@
-// dev env variables
 // TODO #6
-const dotenv = require("dotenv");
-dotenv.config();
-const PORT = process.env.PORT || 4000;
-
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
-const path = require("path");
+const PORT = process.env.PORT || 4000;
 const cors = require("cors")
 
 app.use(cors());
@@ -17,7 +12,6 @@ app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("frontend/build"))
-    // app.use(express.static(path.join(__dirname, 'build')));
 }
 
 app.listen(PORT, () => console.log("hi, your port is ", PORT))
