e49a568eeae09fa90247d0d2754bbff4e630a80a
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-27 09:57:02 -0300

    transforming inputs to get them accepted by api

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -4,16 +4,34 @@ import '../App.css'
 import lupa from '../assets/ic_Search.png'
 import mlbrand from '../assets/Logo_ML.png'
 
+/**
+ * Si no transformo estos caracteres, la api de mercadolibre no me devuelve nada.
+ * @param {String} string 
+ */
+const urlEncoder = string => {
+    let auxString = string.slice();
+    auxString = auxString.replaceAll('á', 'a');
+    auxString = auxString.replaceAll('Á', 'a');
+    auxString = auxString.replaceAll('é', 'e');
+    auxString = auxString.replaceAll('É', 'e');
+    auxString = auxString.replaceAll('í', 'i');
+    auxString = auxString.replaceAll('Í', 'i');
+    auxString = auxString.replaceAll('ó', 'o');
+    auxString = auxString.replaceAll('Ó', 'o');
+    auxString = auxString.replaceAll('ú', 'u');
+    auxString = auxString.replaceAll('Ú', 'u');
+    auxString = auxString.replaceAll('ñ', 'n');
+    return auxString;
+}
+
 export default function SearchBar() {
     // TODO #4
-    // TODO #5
-    // TODO #8
     const [search, setSearch] = useState("");
     let history = useHistory();
     
     const handleSubmit = e => {
         e.preventDefault();
-        history.push(`/items?q=${search}`);
+        history.push(`/items?q=${urlEncoder(search)}`);
     }
         
     return (
